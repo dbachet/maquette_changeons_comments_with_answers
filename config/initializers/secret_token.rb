3770410aef5505916f3b0d7778df70b4f3152c5f
Damien Bachet <onewaytodaf@gmail.com>
2011-10-27 11:11:05 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaquetteChangeonsCommentsWithAnswers::Application.config.secret_token = '1c684530a80b1d356741e0546cba45cdf4a6fab5e9a299eef9b20851cf79358e207bef332ffe0c9144ea51e1b1fd01d7e54715e548949e30a202273dc47d4621'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MaquetteChangeonsCommentsWithAnswers::Application.config.session_store :cookie_store, :key => '_maquette_changeons_comments_with_answers_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MaquetteChangeonsCommentsWithAnswers::Application.config.session_store :active_record_store
